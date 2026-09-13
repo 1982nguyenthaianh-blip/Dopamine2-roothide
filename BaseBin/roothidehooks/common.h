7bf8d375e23a6bd25f9b5b8d465a62e37761e6d9
imaskpro <73496466+imaskpro@users.noreply.github.com>
2026-09-13 13:53:51 +0700

    Pass 8 CI Fix: declare xpc_connection_get_audit_token prototype in common.h

--- BaseBin/roothidehooks/common.h
+++ BaseBin/roothidehooks/common.h
@@ -1,11 +1,16 @@
 
 #include <stdbool.h>
 #include <bsm/libbsm.h>
+#include <xpc/xpc.h>
 
 #include <libjailbreak/libjailbreak.h>
 #include <libjailbreak/jbclient_xpc.h>
 #include <libjailbreak/roothider.h>
 #include <libjailbreak/codesign.h>
+
+void xpc_connection_get_audit_token(xpc_connection_t connection, audit_token_t *token);
+pid_t xpc_connection_get_pid(xpc_connection_t connection);
+uid_t xpc_connection_get_euid(xpc_connection_t connection);
 
 bool isJailbreakBundlePath(const char* path);
 
