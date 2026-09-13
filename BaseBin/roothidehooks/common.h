c476d648d218f2af6965cf5e75db095458e652e5
imaskpro <73496466+imaskpro@users.noreply.github.com>
2026-09-14 03:44:29 +0700

    Use audit_token_to_pid in cfprefsd.x instead of unavailable xpc_connection_get_pid

--- BaseBin/roothidehooks/common.h
+++ BaseBin/roothidehooks/common.h
@@ -1,5 +1,9 @@
 
 #include <stdbool.h>
+#include <bsm/libbsm.h>
+#include <xpc/xpc.h>
+
+void xpc_connection_get_audit_token(xpc_connection_t connection, audit_token_t *token);
 
 #include <libjailbreak/libjailbreak.h>
 #include <libjailbreak/jbclient_xpc.h>
